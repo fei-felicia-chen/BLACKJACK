b565a66d55e85819023fb0b3db0ec593c02b2d35
Fei <81840187+fei-felicia-chen@users.noreply.github.com>
2022-10-24 22:29:20 -0400

    Update BlackJackGame.cpp

--- BlackJackGame.cpp
+++ BlackJackGame.cpp
@@ -59,11 +59,16 @@ int Card::getValue()
     return rank;
 }
 
+/*  This function is used to output player
+ *  hand's card information.
+ *  The output will start with the face 
+ *  value (from 1 to K), followed by 
+ *  the first character of its suit.
+ */
 void Card::displayCard()
 {
-    int displayRank = rank;
+    // Store corresponding type in displayType
     char displayType;
-
     if (type == CLUBS)
         displayType = 'C';
     else if (type == DIAMONDS)
@@ -73,6 +78,8 @@ void Card::displayCard()
     else
         displayType = 'S';
 
+    // Get the corresponding rank in type int and output
+    int displayRank = rank;                         
     if (rank == 11)
         printf("J%c ", displayType);
     else if (rank == 12)
@@ -81,9 +88,12 @@ void Card::displayCard()
         printf("K%c ", displayType);
     else
         printf("%d%c ", displayRank, displayType);
-    // cout << displayRank << displayType << " ";
 }
 
+/*  The Hand class contains a vector of cards
+ *  which indicates what cards a player has in
+ *  its hands.
+ */
 class Hand
 {
 private:
@@ -108,16 +118,33 @@ Hand::~Hand()
     // nothing to add here
 }
 
+/*  This function adds a card to the hand.
+ *  by pushing it onto the vector of cards.
+ *  This is used in the deal function in 
+ *  the Deck class.
+ */
 void Hand::add(Card *card)
 {
     cards.push_back(*card);
 }
 
+/*  This function resets player's hand
+ *  so that they can replay the game.
+ */
 void Hand::clear()
 {
     this->cards.clear();
 }
 
+/*  This function return's the hand's total
+ *  worth by computing the sum of all the cards
+ *  in the player's hand. By the Blackjack game rules,
+ *  an Ace can be worth 1 or 11 points, whichever is
+ *  convenient for the player. From getValue() in Card
+ *  class, all Aces are worth 11 by default and so
+ *  in this function it will decrement the total value
+ *  by 10 for each ace if hand bursts.
+ */
 int Hand::getTotal()
 {
     int numAce = 0;
@@ -131,7 +158,7 @@ int Hand::getTotal()
         total += curr;
     }
 
-    // Take into account number of aces and get total
+    // Take into account number of aces and get total when bursted
     if (total > 21)
     {
         while (total > 21 && numAce-- > 0)
@@ -142,6 +169,7 @@ int Hand::getTotal()
 
 void Hand::displayHand()
 {
+    // Outputs all cards in hand
     for (Card c : cards)
         c.displayCard();
 }
@@ -171,17 +199,18 @@ Deck::~Deck()
 
 void Deck::populate()
 {
-    deck.clear();
-    for (unsigned suit = CLUBS; suit <= SPADES; suit++) {
-        for (unsigned num = ACE; num <= KING; num++) {
+    deck.clear();                                           // empty current deck
+    for (unsigned suit = CLUBS; suit <= SPADES; suit++) {   // iterate through all suits
+        for (unsigned num = ACE; num <= KING; num++) {      // iterate through all ranks
             Card c = Card(Rank(num), Type(suit));
-            deck.push_back(c);
+            deck.push_back(c);                              // add every card to vector
         }
     }
 }
 
 void Deck::shuffle()
 {
+    // Shuffle deck using time as seed
     if (deck.empty())
         return;
     default_random_engine e(chrono::system_clock::now().time_since_epoch().count());
@@ -190,6 +219,7 @@ void Deck::shuffle()
 
 void Deck::deal(Hand *aHand)
 {
+    // Take last card from deck and add to hand
     Card toDeal = deck.back();
     deck.pop_back();
     aHand->add(&toDeal);
@@ -198,7 +228,6 @@ void Deck::deal(Hand *aHand)
 class AbstractPlayer
 {
 protected:
-    virtual bool isDrawing() const = 0;     // check if player wants to draw another card
     Hand *hand;
 
 public:
@@ -227,7 +256,6 @@ class CasinoPlayer: public AbstractPlayer
 public:
     CasinoPlayer();
     ~CasinoPlayer();
-    virtual bool isDrawing() const;
     Hand* getHand();
 };
 
@@ -241,11 +269,6 @@ CasinoPlayer::~CasinoPlayer()
     // nothing
 }
 
-bool CasinoPlayer::isDrawing() const
-{
-    return (this->hand->getTotal() < 16);
-}
-
 Hand* CasinoPlayer::getHand()
 {
     return this->hand;
@@ -256,7 +279,7 @@ class HumanPlayer: public AbstractPlayer
 public:
     HumanPlayer();
     ~HumanPlayer();
-    virtual bool isDrawing() const;
+    bool isDrawing();
     void announce(CasinoPlayer *casino);
     Hand* getHand();
 };
@@ -271,8 +294,9 @@ HumanPlayer::~HumanPlayer()
     // nothing
 }
 
-bool HumanPlayer::isDrawing() const
+bool HumanPlayer::isDrawing()
 {
+    // Take user input to check if user wants to draw
     printf("Do you want to draw? (y/n): ");
     char answer = '!';          // initialize to random character
     cin >> answer;
@@ -362,34 +386,28 @@ void BlackJackGame::play()
     printf("[%d]\n", user_hand->getTotal());
 
     // Deal cards to users
-    bool user_busted = false;
+    bool user_busted = false;                   // bool used to see if casino needs to play
     while (m_user->isDrawing())
     {
-        // Deal another card if no bust
-        if (!(m_user->isBusted()))
-        {
-            m_deck->deal(user_hand);
-            user_hand->displayHand();
-            printf("[%d]\n", user_hand->getTotal());
-            if (m_user->isBusted())
-            {
-                m_user->announce(m_casino);
-                user_busted = true;
-                break;
-            }
-        }
-        // Stop dealing cards when bust
-        else
+        // Deal card
+        m_deck->deal(user_hand);
+        user_hand->displayHand();
+        printf("[%d]\n", user_hand->getTotal());
+
+        // Stop dealing card when user busts
+        if (m_user->isBusted())
         {
             m_user->announce(m_casino);
             user_busted = true;
             break;
         }
+        
     }
 
     // Deal cards to casino player as long as it's less than user's
     while (!user_busted && casino_hand->getTotal() != 21)
     {
+        // Stop dealing when casino wins
         if (casino_hand->getTotal() > user_hand->getTotal())
             break;
         // Deal another card if no bust
@@ -399,7 +417,7 @@ void BlackJackGame::play()
         casino_hand->displayHand();
         printf("[%d]\n", casino_hand->getTotal());
     }
-
+    // Announce winner
     if (!user_busted)
         m_user->announce(m_casino);
 }
@@ -408,15 +426,14 @@ int main()
 {
     printf("\nWelcome to the blackjack table\n\n");
     BlackJackGame game;
-    bool playAgain = true;
-    while (playAgain)
+    while (true)
     {
         game.play();
         printf("Would you like to play another round? (y/n): ");
         char answer = '!';          // initialize to random character
         cin >> answer;
         if (tolower(answer) != 'y')
-            playAgain = false;
+            break;
     }
     printf("Game over!");
     return 0;
